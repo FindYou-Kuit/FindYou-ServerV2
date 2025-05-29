c4566b1956c7e51ad243611abb2909014ccf2e92
ksg <ksg001227@naver.com>
2025-05-29 22:52:38 +0900

    [Init] 프로젝트 초기 설정

---
+++ src/main/java/com/kuit/findyou/FindyouApplication.java
@@ -0,0 +1,13 @@
+package com.kuit.findyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindyouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuit/findyou/FindyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuit.findyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
