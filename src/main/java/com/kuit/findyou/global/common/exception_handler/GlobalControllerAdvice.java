ce2bbb02f320868e9b3b8a8479020c314508a19d
JangIkhwan <bluensky0213@gmail.com>
2025-07-04 14:04:13 +0900

    [Feat] #5 ResponseEntity를 이용하여 전역 예외처리 수정

--- src/main/java/com/kuit/findyou/global/common/exception_handler/GlobalControllerAdvice.java
+++ src/main/java/com/kuit/findyou/global/common/exception_handler/GlobalControllerAdvice.java
@@ -4,10 +4,11 @@ import com.kuit.findyou.global.common.exception.CustomException;
 import com.kuit.findyou.global.common.response.BaseErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.TypeMismatchException;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -18,43 +19,49 @@ import static com.kuit.findyou.global.common.response.status.BaseExceptionRespon
 @RestControllerAdvice
 public class GlobalControllerAdvice {
     // 잘못된 요청일 경우
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(TypeMismatchException.class)
-    public BaseErrorResponse handle_TypeMismatchException(TypeMismatchException e){
+    public ResponseEntity<BaseErrorResponse> handle_TypeMismatchException(TypeMismatchException e){
         log.error("[handle_TypeMismatchException]", e);
-        return new BaseErrorResponse(BAD_REQUEST);
+        return ResponseEntity
+                .badRequest()
+                .body(new BaseErrorResponse(BAD_REQUEST));
     }
 
     // 요청한 api가 없을 경우
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoHandlerFoundException.class)
-    public BaseErrorResponse handle_NoHandlerFoundException(NoHandlerFoundException e){
+    public ResponseEntity<BaseErrorResponse> handle_NoHandlerFoundException(NoHandlerFoundException e){
         log.error("[handle_NoHandlerFoundException]", e);
-        return new BaseErrorResponse(API_NOT_FOUND);
+        return ResponseEntity
+                .badRequest()
+                .body(new BaseErrorResponse(API_NOT_FOUND));
     }
 
     // 런타임 오류가 발생한 경우
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(RuntimeException.class)
-    public BaseErrorResponse handle_RuntimeException(RuntimeException e) {
+    public ResponseEntity<BaseErrorResponse> handle_RuntimeException(RuntimeException e) {
         log.error("[handle_RuntimeException]", e);
-        return new BaseErrorResponse(INTERNAL_SERVER_ERROR);
+        return ResponseEntity
+                .internalServerError()
+                .body(new BaseErrorResponse(INTERNAL_SERVER_ERROR));
     }
 
     // 요청에 필요한 인자가 없는 경우
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public BaseErrorResponse handle_MethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity<BaseErrorResponse> handle_MethodArgumentNotValidException(MethodArgumentNotValidException e) {
         log.error("[handle_MethodArgumentNotValidException]", e);
-        String defaultMessage = e.getBindingResult().getFieldError().getDefaultMessage();
-        return new BaseErrorResponse(BAD_REQUEST, defaultMessage);
+        FieldError fieldError = e.getBindingResult().getFieldError(); // NPE 가능성으로 인해 검증
+        String defaultMessage = (fieldError != null) ? e.getBindingResult().getFieldError().getDefaultMessage() : "Invalid request";
+        return ResponseEntity
+                .badRequest()
+                .body(new BaseErrorResponse(BAD_REQUEST, defaultMessage));
     }
 
     // 커스텀 예외의 경우
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(CustomException.class)
-    public BaseErrorResponse handle_CustomException(CustomException e) {
+    public ResponseEntity<BaseErrorResponse> handle_CustomException(CustomException e) {
         log.error("[handle_CustomException]", e);
-        return new BaseErrorResponse(e.getExceptionStatus());
+        return ResponseEntity
+                .status(HttpStatusCode.valueOf(e.getExceptionStatus().getCode()))
+                .body(new BaseErrorResponse(e.getExceptionStatus()));
     }
 }
