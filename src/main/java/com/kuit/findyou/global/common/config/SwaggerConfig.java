df0abab52826043ab2ae692dc5641dae0dca045f
ksg <ksg001227@naver.com>
2025-07-09 15:34:48 +0900

    [Feat] #7 Swagger 응답 구조를 상세하게 변경

--- src/main/java/com/kuit/findyou/global/common/config/SwaggerConfig.java
+++ src/main/java/com/kuit/findyou/global/common/config/SwaggerConfig.java
@@ -1,10 +1,30 @@
 package com.kuit.findyou.global.common.config;
 
+import com.kuit.findyou.global.common.annotation.CustomExceptionDescription;
+import com.kuit.findyou.global.common.response.BaseErrorResponse;
+import com.kuit.findyou.global.common.response.status.BaseExceptionResponseStatus;
+import com.kuit.findyou.global.common.swagger.ExampleHolder;
+import com.kuit.findyou.global.common.swagger.SwaggerResponseDescription;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.examples.Example;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.stream.Collectors.groupingBy;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -19,5 +39,75 @@ public class SwaggerConfig {
         @Bean
         public OpenAPI openAPI() {
                 return new OpenAPI();
+        }
+
+        @Bean
+        public OperationCustomizer customize() {
+                return (Operation operation, HandlerMethod handlerMethod) -> {
+
+                        CustomExceptionDescription customExceptionDescription = handlerMethod.getMethodAnnotation(
+                                CustomExceptionDescription.class);
+
+                        // CustomExceptionDescription 어노테이션 단 메소드 적용
+                        if (customExceptionDescription != null) {
+                                generateErrorCodeResponseExample(operation, customExceptionDescription.value());
+                        }
+
+                        return operation;
+                };
+        }
+
+
+        private void generateErrorCodeResponseExample(
+                Operation operation, SwaggerResponseDescription type) {
+
+                ApiResponses responses = operation.getResponses();
+
+                Set<BaseExceptionResponseStatus> baseExceptionResponseStatusSet = type.getExceptionResponseStatusSet();
+
+                Map<Integer, List<ExampleHolder>> statusWithExampleHolders =
+                        baseExceptionResponseStatusSet.stream()
+                                .map(
+                                        baseExceptionResponseStatus -> {
+                                                return ExampleHolder.builder()
+                                                        .holder(
+                                                                getSwaggerExample(baseExceptionResponseStatus))
+                                                        .code(baseExceptionResponseStatus.getCode())
+                                                        .name(baseExceptionResponseStatus.toString())
+                                                        .build();
+                                        }
+                                ).collect(groupingBy(ExampleHolder::getCode));
+                addExamplesToResponses(responses, statusWithExampleHolders);
+        }
+
+
+        private Example getSwaggerExample(BaseExceptionResponseStatus baseExceptionResponseStatus) {
+                BaseErrorResponse errorResponse = new BaseErrorResponse(baseExceptionResponseStatus);
+
+                Example example = new Example();
+                example.description(baseExceptionResponseStatus.getMessage());
+                example.setValue(errorResponse);
+
+                return example;
+        }
+
+
+        private void addExamplesToResponses(
+                ApiResponses responses, Map<Integer, List<ExampleHolder>> statusWithExampleHolders) {
+                statusWithExampleHolders.forEach(
+                        (status, v) -> {
+                                Content content = new Content();
+                                MediaType mediaType = new MediaType();
+                                ApiResponse apiResponse = new ApiResponse();
+                                v.forEach(
+                                        exampleHolder -> {
+                                                mediaType.addExamples(
+                                                        exampleHolder.getName(), exampleHolder.getHolder());
+                                        });
+                                content.addMediaType("application/json", mediaType);
+                                apiResponse.setDescription("");
+                                apiResponse.setContent(content);
+                                responses.addApiResponse(status.toString(), apiResponse);
+                        });
         }
 }
--- src/main/java/com/kuit/findyou/global/common/response/status/BaseExceptionResponseStatus.java
+++ src/main/java/com/kuit/findyou/global/common/response/status/BaseExceptionResponseStatus.java
@@ -1,12 +1,18 @@
 package com.kuit.findyou.global.common.response.status;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum BaseExceptionResponseStatus implements ResponseStatus{
     SUCCESS(200, "요청에 성공했습니다."),
+
+    // 공통 에러
     BAD_REQUEST(400, "유효하지 않은 요청입니다."),
+    UNAUTHORIZED(401, "인증 자격이 없습니다."),
+    FORBIDDEN(403, "권한이 없습니다."),
     API_NOT_FOUND(404, "존재하지 않는 API입니다."),
+    METHOD_NOT_ALLOWED(405, "유효하지 않은 Http 메서드입니다."),
     INTERNAL_SERVER_ERROR(500, "서버 내부 오류입니다.");
 
     private final boolean success = false;
