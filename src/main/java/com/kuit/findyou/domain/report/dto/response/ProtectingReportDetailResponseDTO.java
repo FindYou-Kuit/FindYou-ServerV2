e4fb75a532e3984ace690c1d21117cbd354d596d
ksg <ksg001227@naver.com>
2025-07-10 18:18:20 +0900

    [Feat] #9 보호글 상세 조회용 DTO 구현

---
+++ src/main/java/com/kuit/findyou/domain/report/dto/response/ProtectingReportDetailResponseDTO.java
@@ -0,0 +1,27 @@
+package com.kuit.findyou.domain.report.dto.response;
+
+import java.util.List;
+
+public record ProtectingReportDetailResponseDTO(
+        List<String> imageUrl,
+        String breed,
+        String tag,
+        String age,
+        String weight,
+        String furColor,
+        String sex,
+        String neutering,
+        String significant,
+        String careName,
+        String careAddr,
+        double latitude,
+        double longitude,
+        String careTel,
+        String foundDate,
+        String foundLocation,
+        String noticeDuration,
+        String noticeNumber,
+        String authority,
+        boolean interest
+) {
+}
