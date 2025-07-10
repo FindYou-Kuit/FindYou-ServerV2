7580f8255d55c508e337ba44df2abd95a7b25e93
ksg <ksg001227@naver.com>
2025-07-10 18:23:59 +0900

    [Feat] #9 실종 신고글 상세 조회용 DTO 구현

---
+++ src/main/java/com/kuit/findyou/domain/report/dto/response/MissingReportDetailResponseDTO.java
@@ -0,0 +1,21 @@
+package com.kuit.findyou.domain.report.dto.response;
+
+import java.util.List;
+
+public record MissingReportDetailResponseDTO(
+        List<String> imageUrls,
+        String breed,
+        String tag,
+        String age,
+        String sex,
+        String missingDate,
+        String rfid,
+        String significant,
+        String missingLocation,
+        String missingAddress,
+        double latitude,
+        double longitude,
+        String reporterInfo,
+        boolean interest
+) {
+}
