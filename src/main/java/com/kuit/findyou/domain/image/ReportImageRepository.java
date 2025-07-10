fa2612880037ed0d11cb779b4297b902847c5a77
ksg <ksg001227@naver.com>
2025-07-10 20:23:37 +0900

    [Feat] #9 ReportImageRepository 구현

---
+++ src/main/java/com/kuit/findyou/domain/image/ReportImageRepository.java
@@ -0,0 +1,7 @@
+package com.kuit.findyou.domain.image;
+
+import com.kuit.findyou.domain.image.model.ReportImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportImageRepository extends JpaRepository<ReportImage, Long> {
+}
