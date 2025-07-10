cdfd80cd9795ddbbd22d6bd672a489f1324b2de4
ksg <ksg001227@naver.com>
2025-07-10 20:47:20 +0900

    [Refactor] #9 GlobalControllerAdvice 의 반환 타입을 BaseErrorResponse 로 변경

--- src/main/java/com/kuit/findyou/global/common/exception_handler/GlobalControllerAdvice.java
+++ src/main/java/com/kuit/findyou/global/common/exception_handler/GlobalControllerAdvice.java
@@ -2,6 +2,7 @@ package com.kuit.findyou.global.common.exception_handler;
 
 import com.kuit.findyou.global.common.exception.CustomException;
 import com.kuit.findyou.global.common.response.BaseErrorResponse;
+import com.kuit.findyou.global.common.response.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.TypeMismatchException;
 import org.springframework.http.HttpStatusCode;
@@ -20,48 +21,38 @@ import static com.kuit.findyou.global.common.response.status.BaseExceptionRespon
 public class GlobalControllerAdvice {
     // 잘못된 요청일 경우
     @ExceptionHandler(TypeMismatchException.class)
-    public ResponseEntity<BaseErrorResponse> handle_TypeMismatchException(TypeMismatchException e){
+    public BaseErrorResponse handle_TypeMismatchException(TypeMismatchException e){
         log.error("[handle_TypeMismatchException]", e);
-        return ResponseEntity
-                .badRequest()
-                .body(new BaseErrorResponse(BAD_REQUEST));
+        return new BaseErrorResponse(BAD_REQUEST);
     }
 
     // 요청한 api가 없을 경우
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<BaseErrorResponse> handle_NoHandlerFoundException(NoHandlerFoundException e){
+    public BaseErrorResponse handle_NoHandlerFoundException(NoHandlerFoundException e){
         log.error("[handle_NoHandlerFoundException]", e);
-        return ResponseEntity
-                .badRequest()
-                .body(new BaseErrorResponse(API_NOT_FOUND));
+        return new BaseErrorResponse(API_NOT_FOUND);
     }
 
     // 런타임 오류가 발생한 경우
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<BaseErrorResponse> handle_RuntimeException(RuntimeException e) {
+    public BaseErrorResponse handle_RuntimeException(RuntimeException e) {
         log.error("[handle_RuntimeException]", e);
-        return ResponseEntity
-                .internalServerError()
-                .body(new BaseErrorResponse(INTERNAL_SERVER_ERROR));
+        return new BaseErrorResponse(INTERNAL_SERVER_ERROR);
     }
 
     // 요청에 필요한 인자가 없는 경우
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<BaseErrorResponse> handle_MethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public BaseErrorResponse handle_MethodArgumentNotValidException(MethodArgumentNotValidException e) {
         log.error("[handle_MethodArgumentNotValidException]", e);
         FieldError fieldError = e.getBindingResult().getFieldError(); // NPE 가능성으로 인해 검증
         String defaultMessage = (fieldError != null) ? e.getBindingResult().getFieldError().getDefaultMessage() : "Invalid request";
-        return ResponseEntity
-                .badRequest()
-                .body(new BaseErrorResponse(BAD_REQUEST, defaultMessage));
+        return new BaseErrorResponse(BAD_REQUEST, defaultMessage);
     }
 
     // 커스텀 예외의 경우
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<BaseErrorResponse> handle_CustomException(CustomException e) {
+    public BaseErrorResponse handle_CustomException(CustomException e) {
         log.error("[handle_CustomException]", e);
-        return ResponseEntity
-                .status(HttpStatusCode.valueOf(e.getExceptionStatus().getCode()))
-                .body(new BaseErrorResponse(e.getExceptionStatus()));
+        return new BaseErrorResponse(e.getExceptionStatus());
     }
 }
