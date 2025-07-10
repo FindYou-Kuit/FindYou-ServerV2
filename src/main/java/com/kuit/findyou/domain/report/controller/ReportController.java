477d080ecf2f3066c189f899bb6422accd88da9e
ksg <ksg001227@naver.com>
2025-07-10 20:30:46 +0900

    [Feat] #9 API 들에 대한 설명 추가

--- src/main/java/com/kuit/findyou/domain/report/controller/ReportController.java
+++ src/main/java/com/kuit/findyou/domain/report/controller/ReportController.java
@@ -7,6 +7,8 @@ import com.kuit.findyou.domain.report.model.*;
 import com.kuit.findyou.domain.report.service.ReportDetailService;
 import com.kuit.findyou.global.common.annotation.CustomExceptionDescription;
 import com.kuit.findyou.global.common.response.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static com.kuit.findyou.global.common.swagger.SwaggerResponseDescription.*;
 
+@Tag(name = "Report", description = "글 조회 API")
 @RestController
 @Slf4j
 @RequestMapping("/api/v2/reports")
@@ -25,6 +28,10 @@ public class ReportController {
     private final ReportDetailService reportDetailService;
 
     // todo userId 를 토큰으로부터 추출하는 로직 필요
+    @Operation(
+            summary = "보호글 상세 조회 API",
+            description = "보호글의 정보를 상세 조회하기 위한 API"
+    )
     @GetMapping("/protecting/{reportId}")
     @CustomExceptionDescription(PROTECTING_REPORT_DETAIL)
     public BaseResponse<ProtectingReportDetailResponseDTO> getProtectingReportDetail(
@@ -35,6 +42,10 @@ public class ReportController {
     }
 
     // todo userId 를 토큰으로부터 추출하는 로직 필요
+    @Operation(
+            summary = "실종 신고글 상세 조회 API",
+            description = "실종 신고글의 정보를 상세 조회하기 위한 API"
+    )
     @GetMapping("/missing/{reportId}")
     @CustomExceptionDescription(MISSING_REPORT_DETAIL)
     public BaseResponse<MissingReportDetailResponseDTO> getMissingReportDetail(
@@ -45,6 +56,10 @@ public class ReportController {
     }
 
     // todo userId 를 토큰으로부터 추출하는 로직 필요
+    @Operation(
+            summary = "목격 신고글 상세 조회 API",
+            description = "목격 신고글의 정보를 상세 조회하기 위한 API"
+    )
     @GetMapping("/witness/{reportId}")
     @CustomExceptionDescription(WITNESS_REPORT_DETAIL)
     public BaseResponse<WitnessReportDetailResponseDTO> getWitnessReportDetail(
