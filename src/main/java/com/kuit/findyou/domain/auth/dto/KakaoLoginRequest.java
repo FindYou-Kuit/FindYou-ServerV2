bdac8aa38269476d8350899043891555f1ab5a9c
JangIkhwan <bluensky0213@gmail.com>
2025-07-11 20:01:58 +0900

    [Docs] #12 카카오 로그인 API에 대해 Swagger 명세 추가

--- src/main/java/com/kuit/findyou/domain/auth/dto/KakaoLoginRequest.java
+++ src/main/java/com/kuit/findyou/domain/auth/dto/KakaoLoginRequest.java
@@ -1,6 +1,10 @@
 package com.kuit.findyou.domain.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "로그인 요청 DTO")
 public record KakaoLoginRequest(
+        @Schema(description = "사용자 카카오 ID", example = "12345678")
         Long kakaoId
 ) {
 }
