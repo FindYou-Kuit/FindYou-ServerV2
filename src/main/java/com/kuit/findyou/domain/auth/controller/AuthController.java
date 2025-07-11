c4cf43dd829de917114da2a525eb334ed32e918c
JangIkhwan <bluensky0213@gmail.com>
2025-07-11 17:37:29 +0900

    [Feat] #12 컨트롤러에 카카오 로그인 메서드를 구현

--- src/main/java/com/kuit/findyou/domain/auth/controller/AuthController.java
+++ src/main/java/com/kuit/findyou/domain/auth/controller/AuthController.java
@@ -1,11 +1,25 @@
 package com.kuit.findyou.domain.auth.controller;
 
+import com.kuit.findyou.domain.auth.dto.KakaoLoginRequest;
+import com.kuit.findyou.domain.auth.dto.KakaoLoginResponse;
+import com.kuit.findyou.domain.auth.service.AuthService;
+import com.kuit.findyou.global.common.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RequestMapping("auth")
+@RequiredArgsConstructor
+@RequestMapping("api/v2/auth")
 @RestController
 public class AuthController {
+    private final AuthService authService;
+    @PostMapping("login/kakao")
+    public BaseResponse<KakaoLoginResponse> kakaoLogin(@RequestBody KakaoLoginRequest request){
+        log.info("[kakaoLogin]");
+        return BaseResponse.ok(authService.kakaoLogin(request));
+    }
 }
