b5bdca987106972b6db4d264db92d9bcc2ee8da4
JangIkhwan <bluensky0213@gmail.com>
2025-07-11 19:39:42 +0900

    [Feat] #12 카카오로그인 응답 DTO에 객체 생성용 정적 메서드를 추가

--- src/main/java/com/kuit/findyou/domain/auth/dto/KakaoLoginResponse.java
+++ src/main/java/com/kuit/findyou/domain/auth/dto/KakaoLoginResponse.java
@@ -1,7 +1,17 @@
 package com.kuit.findyou.domain.auth.dto;
 
+import com.kuit.findyou.domain.user.model.User;
+
 public record KakaoLoginResponse(
         UserInfoDto userInfo,
         Boolean isFirstLogin
 ) {
+    public static KakaoLoginResponse fromUserAndAccessToken(User user, String accessToken) {
+        UserInfoDto userInfo = new UserInfoDto(user.getId(), user.getName(), accessToken);
+        return new KakaoLoginResponse(userInfo, true);
+    }
+
+    public static KakaoLoginResponse notFound() {
+        return new KakaoLoginResponse(null, true);
+    }
 }
