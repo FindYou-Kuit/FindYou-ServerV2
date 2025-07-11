75a6ae64bf10408d0f77ecb4bf6999b231de4c3c
JJUYAAA <jjjjyy020704@naver.com>
2025-07-11 17:48:14 +0900

    [Feat] #13 구독 entity

---
+++ src/main/java/com/kuit/findyou/domain/subscribe/model/Subscribe.java
@@ -0,0 +1,32 @@
+package com.kuit.findyou.domain.subscribe.model;
+
+import com.kuit.findyou.domain.keyword.model.Keyword;
+import com.kuit.findyou.domain.user.model.User;
+import com.kuit.findyou.global.common.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Table(name = "subscribes")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@SQLDelete(sql = "UPDATE subscribes SET status = 'N' WHERE subscribe_id = ?")
+@SQLRestriction(value = "status = 'Y'")
+public class Subscribe extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subscribe_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "keyword_id", nullable = false)
+    private Keyword keyword;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
