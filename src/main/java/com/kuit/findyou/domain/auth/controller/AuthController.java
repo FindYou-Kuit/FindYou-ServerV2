34a2ff83bc5172ef03fd05e1b86dd162ff40db18
JangIkhwan <bluensky0213@gmail.com>
2025-07-11 17:01:59 +0900

    [Feat] #12 AuthController 추가

---
+++ src/main/java/com/kuit/findyou/domain/auth/controller/AuthController.java
@@ -0,0 +1,11 @@
+package com.kuit.findyou.domain.auth.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("auth")
+@RestController
+public class AuthController {
+}
