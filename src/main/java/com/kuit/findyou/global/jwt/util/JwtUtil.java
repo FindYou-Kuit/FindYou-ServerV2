b48556bd08cf20d13e9c38158c2663840d107945
JangIkhwan <bluensky0213@gmail.com>
2025-07-11 19:34:39 +0900

    [Feat] #12 JwtUtil 구현

---
+++ src/main/java/com/kuit/findyou/global/jwt/util/JwtUtil.java
@@ -0,0 +1,64 @@
+package com.kuit.findyou.global.jwt.util;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Slf4j
+@Component
+public class JwtUtil {
+    private final SecretKey secretKey;
+
+    @Value("${findyou.jwt.access.expire-ms}")
+    private long accessTokenExpireMs;
+
+    private static final String ACCESS_TOKEN = "accessToken";
+
+    public JwtUtil(@Value("${findyou.jwt.secret-key}") String secret) {
+        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public Long getUserId(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("userId", Long.class);
+    }
+
+    public String getTokenType(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("tokenType", String.class);
+    }
+
+    public String createAccessJwt(Long userId) {
+        return Jwts.builder()
+                .claim("userId", userId)
+                .claim("tokenType", ACCESS_TOKEN)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + accessTokenExpireMs))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public boolean validateJwt(String token){
+        log.info("validateJwt");
+        try{
+            Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token);
+            return true;
+        } catch (MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty.", e);
+        }
+        return false;
+    }
+}
