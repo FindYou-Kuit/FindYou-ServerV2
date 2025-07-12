7014cfcab040ff2b7fbad6eff88a614d5d65ddfd
ksg <ksg001227@naver.com>
2025-07-13 00:23:45 +0900

    [Refactor] #9 도메인별 컨트롤러는 Facade 클래스 하나만 의존하도록 구현 변경

--- src/main/java/com/kuit/findyou/domain/report/controller/ReportController.java
+++ src/main/java/com/kuit/findyou/domain/report/controller/ReportController.java
@@ -4,7 +4,7 @@ import com.kuit.findyou.domain.report.dto.response.MissingReportDetailResponseDT
 import com.kuit.findyou.domain.report.dto.response.ProtectingReportDetailResponseDTO;
 import com.kuit.findyou.domain.report.dto.response.WitnessReportDetailResponseDTO;
 import com.kuit.findyou.domain.report.model.*;
-import com.kuit.findyou.domain.report.service.ReportDetailService;
+import com.kuit.findyou.domain.report.service.facade.ReportServiceFacade;
 import com.kuit.findyou.global.common.annotation.CustomExceptionDescription;
 import com.kuit.findyou.global.common.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,55 +18,42 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static com.kuit.findyou.global.common.swagger.SwaggerResponseDescription.*;
 
-@Tag(name = "Report", description = "글 조회 API")
 @RestController
 @Slf4j
 @RequestMapping("/api/v2/reports")
+@Tag(name = "Report", description = "글 조회 API")
 @RequiredArgsConstructor
 public class ReportController {
 
-    private final ReportDetailService reportDetailService;
+    private final ReportServiceFacade reportServiceFacade;
 
-    // todo userId 를 토큰으로부터 추출하는 로직 필요
-    @Operation(
-            summary = "보호글 상세 조회 API",
-            description = "보호글의 정보를 상세 조회하기 위한 API"
-    )
+    @Operation(summary = "보호글 상세 조회 API", description = "보호글의 정보를 상세 조회하기 위한 API")
     @GetMapping("/protecting/{reportId}")
     @CustomExceptionDescription(PROTECTING_REPORT_DETAIL)
     public BaseResponse<ProtectingReportDetailResponseDTO> getProtectingReportDetail(
             @PathVariable("reportId") Long reportId) {
 
-        ProtectingReportDetailResponseDTO detail = reportDetailService.getReportDetail(ReportTag.PROTECTING, reportId, 1L);
+        ProtectingReportDetailResponseDTO detail = reportServiceFacade.getReportDetail(ReportTag.PROTECTING, reportId, 1L);
         return BaseResponse.ok(detail);
     }
 
-    // todo userId 를 토큰으로부터 추출하는 로직 필요
-    @Operation(
-            summary = "실종 신고글 상세 조회 API",
-            description = "실종 신고글의 정보를 상세 조회하기 위한 API"
-    )
+    @Operation(summary = "실종 신고글 상세 조회 API", description = "실종 신고글의 정보를 상세 조회하기 위한 API")
     @GetMapping("/missing/{reportId}")
     @CustomExceptionDescription(MISSING_REPORT_DETAIL)
     public BaseResponse<MissingReportDetailResponseDTO> getMissingReportDetail(
             @PathVariable("reportId") Long reportId) {
 
-        MissingReportDetailResponseDTO detail = reportDetailService.getReportDetail(ReportTag.MISSING, reportId, 1L);
+        MissingReportDetailResponseDTO detail = reportServiceFacade.getReportDetail(ReportTag.MISSING, reportId, 1L);
         return BaseResponse.ok(detail);
     }
 
-    // todo userId 를 토큰으로부터 추출하는 로직 필요
-    @Operation(
-            summary = "목격 신고글 상세 조회 API",
-            description = "목격 신고글의 정보를 상세 조회하기 위한 API"
-    )
+    @Operation(summary = "목격 신고글 상세 조회 API", description = "목격 신고글의 정보를 상세 조회하기 위한 API")
     @GetMapping("/witness/{reportId}")
     @CustomExceptionDescription(WITNESS_REPORT_DETAIL)
     public BaseResponse<WitnessReportDetailResponseDTO> getWitnessReportDetail(
             @PathVariable("reportId") Long reportId) {
 
-        WitnessReportDetailResponseDTO detail = reportDetailService.getReportDetail(ReportTag.WITNESS, reportId, 1L);
+        WitnessReportDetailResponseDTO detail = reportServiceFacade.getReportDetail(ReportTag.WITNESS, reportId, 1L);
         return BaseResponse.ok(detail);
     }
-
 }
