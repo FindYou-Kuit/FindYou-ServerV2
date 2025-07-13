a9b53b9e0acb382150fa814682bc72481df2909b
JangIkhwan <bluensky0213@gmail.com>
2025-07-13 23:58:09 +0900

    [Feat] #12 로그인한 사용자의 id를 전달하는 argument resolver 구현

---
+++ src/main/java/com/kuit/findyou/global/jwt/argument_resolver/LoginUserIdArgumentResolver.java
@@ -0,0 +1,31 @@
+package com.kuit.findyou.global.jwt.argument_resolver;
+
+import com.kuit.findyou.global.jwt.annotation.LoginUserId;
+import com.kuit.findyou.global.jwt.util.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Slf4j
+@RequiredArgsConstructor
+public class LoginUserIdArgumentResolver implements HandlerMethodArgumentResolver {
+    private final JwtUtil jwtUtil;
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(Long.class) && parameter.hasParameterAnnotation(LoginUserId.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String token = (String) request.getAttribute("token");
+        Long userId = jwtUtil.getUserId(token);
+        log.info("userId = {}",  userId);
+        return userId;
+    }
+}
