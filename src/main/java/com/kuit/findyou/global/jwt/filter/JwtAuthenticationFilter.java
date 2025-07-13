6f566a3360d94245e74ab500319c0ab426386dbf
JangIkhwan <bluensky0213@gmail.com>
2025-07-13 22:36:36 +0900

    [Feat] #12 JWT 기반 인가 처리 필터 구현

---
+++ src/main/java/com/kuit/findyou/global/jwt/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.kuit.findyou.global.jwt.filter;
+
+import com.kuit.findyou.domain.user.model.Role;
+import com.kuit.findyou.domain.user.model.User;
+import com.kuit.findyou.global.jwt.security.CustomUserDetails;
+import com.kuit.findyou.global.jwt.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = resolveToken(request);
+        if(token != null || jwtUtil.validateJwt(token)){
+            // UserDetails 생성
+            Long userId = jwtUtil.getUserId(token);
+            Role role = jwtUtil.getRole(token);
+
+            // CustomUserDetailsService를 거치지 않고 바로 인증 객체 생성
+            User user = User.builder()
+                    .id(userId)
+                    .role(role)
+                    .build();
+            CustomUserDetails customUserDetails = new CustomUserDetails(user);
+
+            //스프링 시큐리티 인증 객체를 생성하고 컨텍스트에 저장
+            Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+
+        // 다음 필터로 진행
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request){
+        String authorization = request.getHeader("Authorization");
+        if(authorization != null && authorization.startsWith("Bearer ")){
+            String token = authorization.split(" ")[1];
+            return token;
+        }
+        log.info("token does not exist!");
+        return null;
+    }
+}
