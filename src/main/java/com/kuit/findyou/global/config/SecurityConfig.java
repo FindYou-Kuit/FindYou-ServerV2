2d0a630b1490b3eddf0545909208314afa625666
JangIkhwan <bluensky0213@gmail.com>
2025-07-13 22:39:17 +0900

    [Feat] #12 시큐리티 설정 클래스 구현

---
+++ src/main/java/com/kuit/findyou/global/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.kuit.findyou.global.config;
+
+import com.kuit.findyou.global.jwt.entrypoint.CustomAuthenticationEntryPoint;
+import com.kuit.findyou.global.jwt.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final String[] PERMIT_URL = {
+            "/api/v2/auth/**", "/swagger-ui/**", "/api-docs", "/swagger-ui-custom.html",
+            "/v3/api-docs/**", "/api-docs/**", "/swagger-ui.html", "/swagger-ui/index.html"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http
+                .csrf((auth) -> auth.disable());
+
+        http
+                .formLogin((auth)->auth.disable());
+
+        http
+                .httpBasic((auth)->auth.disable());
+
+        // 토큰 기반 인증 비활성화
+//        http
+//                .authorizeHttpRequests((auth)-> auth
+//                        .anyRequest().permitAll());
+
+        // 토큰 기반 인증 활성화
+        http
+                .authorizeHttpRequests((auth)-> auth
+                        .requestMatchers(PERMIT_URL).permitAll()
+                        .anyRequest().authenticated());
+
+        // 토큰 검증 필터 추가
+        http
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        http
+                .sessionManagement((session)->session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
