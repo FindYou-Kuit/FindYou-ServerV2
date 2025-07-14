a2303f5910386416bf962ca9ecc39aa1b5093b08
ksg <ksg001227@naver.com>
2025-07-15 04:17:42 +0900

    [Chore] #17 어노테이션 위치 통일

--- src/main/java/com/kuit/findyou/domain/report/controller/ReportController.java
+++ src/main/java/com/kuit/findyou/domain/report/controller/ReportController.java
@@ -60,8 +60,8 @@ public class ReportController {
     }
 
     @Operation(summary = "글 조회 API (전체 / 구조 동물 / 신고 동물)", description = "글 조회를 위한 API - 전체 조회/구조 동물 조회/신고 동물 조회 시 쿼리 파라미터로 케이스를 구분")
-    @CustomExceptionDescription(RETRIEVE_REPORTS)
     @GetMapping
+    @CustomExceptionDescription(RETRIEVE_REPORTS)
     public BaseResponse<CardResponseDTO> retrieveReportsWithFilters(
             @RequestParam ReportViewType type,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
