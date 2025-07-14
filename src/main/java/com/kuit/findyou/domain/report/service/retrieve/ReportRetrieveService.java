81ccbf75d92912de8684d89c4d9ce29a6467c61b
ksg <ksg001227@naver.com>
2025-07-14 20:05:58 +0900

    [Feat] #17 글 조회 API 용 인터페이스 구현

--- src/main/java/com/kuit/findyou/domain/report/service/retrieve/ReportRetrieveService.java
+++ src/main/java/com/kuit/findyou/domain/report/service/retrieve/ReportRetrieveService.java
@@ -1,6 +1,19 @@
 package com.kuit.findyou.domain.report.service.retrieve;
 
+import com.kuit.findyou.domain.report.dto.response.CardResponseDTO;
+import com.kuit.findyou.domain.report.model.ReportTag;
+
+import java.time.LocalDate;
+
 public interface ReportRetrieveService {
+
+    public CardResponseDTO retrieveReportsWithFilters(Long lastReportId,
+                                                      ReportTag reportTag,
+                                                      LocalDate startDate,
+                                                      LocalDate endDate,
+                                                      String species,
+                                                      String breeds,
+                                                      String location);
 
 
 }
