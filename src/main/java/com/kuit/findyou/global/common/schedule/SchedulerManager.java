8b8f6881d425da95abe60d7160a75896375ec509
ksg <ksg001227@naver.com>
2025-07-20 02:17:10 +0900

    [Feat] #20 구조동물 데이터 동기화를 매일 새벽 4시에 수행할 수 있도록 스케줄링 설정

---
+++ src/main/java/com/kuit/findyou/global/common/schedule/SchedulerManager.java
@@ -0,0 +1,23 @@
+package com.kuit.findyou.global.common.schedule;
+
+import com.kuit.findyou.global.common.external.client.ProtectingAnimalApiClient;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SchedulerManager {
+
+    private final ProtectingAnimalApiClient protectingAnimalApiClient;
+
+    /**
+     * 구조 동물 데이터를 매일 새벽 4시에 동기화
+     */
+    @Scheduled(cron = "0 0 4 * * *")
+    public void syncProtectingAnimals() {
+        protectingAnimalApiClient.storeProtectingReports();
+    }
+}
