bb9dad4d9ec7751935714d43abb58c2bf34dce08
ksg <ksg001227@naver.com>
2025-07-25 02:16:05 +0900

    [Feat] #25 통합 테스트들 간의 독립성을 보장하기 위해 DB를 초기화해주는 DatabaseCleaner 도입

---
+++ src/test/java/com/kuit/findyou/global/common/util/DatabaseCleaner.java
@@ -0,0 +1,41 @@
+package com.kuit.findyou.global.common.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Table;
+import jakarta.persistence.metamodel.Type;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Component
+public class DatabaseCleaner {
+
+    private final EntityManager entityManager;
+    private final List<String> tableNames;
+
+    public DatabaseCleaner(final EntityManager entityManager) {
+        this.entityManager = entityManager;
+        this.tableNames = entityManager.getMetamodel()
+                .getEntities()
+                .stream()
+                .map(Type::getJavaType)
+                .map(javaType -> javaType.getAnnotation(Table.class))
+                .map(Table::name)
+                .toList();
+    }
+
+    @Transactional
+    public void execute() {
+        entityManager.flush();
+
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+
+        for (String tableName : tableNames) {
+            entityManager.createNativeQuery("TRUNCATE TABLE " + tableName + " RESTART IDENTITY").executeUpdate();
+        }
+
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+    }
+}
