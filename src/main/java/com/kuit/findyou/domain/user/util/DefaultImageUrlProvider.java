8012f5af3b8d56e2ebe1d428efdc5280fa7d8553
JangIkhwan <bluensky0213@gmail.com>
2025-07-24 21:43:14 +0900

    [Feat] #23 기본 이미지 이름을 URL로 변환하는 유틸리티 구현

---
+++ src/main/java/com/kuit/findyou/domain/user/util/DefaultImageUrlProvider.java
@@ -0,0 +1,32 @@
+package com.kuit.findyou.domain.user.util;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "findyou.default-images")
+public class DefaultImageUrlProvider {
+    private Map<String, String> defaultImageUrlMap;
+
+    @PostConstruct
+    public void validate() {
+        if (defaultImageUrlMap == null || defaultImageUrlMap.isEmpty()) {
+            throw new IllegalStateException("defaultImageUrlMap 설정이 비어있습니다. application.yml을 확인하세요!");
+        }
+    }
+
+    public String getImageUrl(String name) {
+        String imageUrl = defaultImageUrlMap.get(name);
+        if (imageUrl == null){
+            throw new IllegalArgumentException("존재하지 않는 키값입니다");
+        }
+        return imageUrl;
+    }
+
+    public boolean containsKey(String name){
+        return defaultImageUrlMap.containsKey(name);
+    }
+}
