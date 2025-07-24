23f8e91a7340950cf7aa0877d49fc56627e843e3
JangIkhwan <bluensky0213@gmail.com>
2025-07-25 00:20:05 +0900

    [Feat] #23 빌더 추가

--- src/main/java/com/kuit/findyou/domain/user/dto/RegisterUserRequest.java
+++ src/main/java/com/kuit/findyou/domain/user/dto/RegisterUserRequest.java
@@ -1,7 +1,9 @@
 package com.kuit.findyou.domain.user.dto;
 
+import lombok.Builder;
 import org.springframework.web.multipart.MultipartFile;
 
+@Builder
 public record RegisterUserRequest(
         MultipartFile profileImage,
         String defaultProfileImageName,
