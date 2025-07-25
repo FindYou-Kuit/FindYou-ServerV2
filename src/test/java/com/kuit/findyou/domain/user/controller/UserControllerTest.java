f81a6123b494fa0ac6c9529c6041cfe71b64d98d
ksg <ksg001227@naver.com>
2025-07-25 19:11:00 +0900

    [Test] #25 UserController 통합 테스트 구현

---
+++ src/test/java/com/kuit/findyou/domain/user/controller/UserControllerTest.java
@@ -0,0 +1,85 @@
+package com.kuit.findyou.domain.user.controller;
+
+import com.kuit.findyou.domain.user.model.User;
+import com.kuit.findyou.global.common.util.DatabaseCleaner;
+import com.kuit.findyou.global.common.util.TestInitializer;
+import com.kuit.findyou.global.jwt.util.JwtUtil;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDate;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
+class UserControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    TestInitializer testInitializer;
+
+    @Autowired
+    DatabaseCleaner databaseCleaner;
+
+    @Autowired
+    JwtUtil jwtUtil;
+
+    User reportWriter;
+
+    @BeforeAll
+    void setUp() {
+        databaseCleaner.execute();
+
+        RestAssured.port = port;
+        testInitializer.initializeControllerTestData();
+        this.reportWriter = testInitializer.getReportWriter();
+    }
+
+    @Test
+    @DisplayName("GET /api/v2/users/me/viewed-animals: 최근 본 글 조회 성공")
+    void retrieveViewedAnimals() {
+        // 작성자의 엑세스 토큰 생성
+        String accessToken = jwtUtil.createAccessJwt(reportWriter.getId(), reportWriter.getRole());
+
+        given()
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .param("lastId", Long.MAX_VALUE)
+        .when()
+                .get("/api/v2/users/me/viewed-animals")
+        .then()
+                .statusCode(200)
+                .body("data.cards[0].reportId", equalTo(2))
+                .body("data.cards[0].thumbnailImageUrl", equalTo("https://img.com/missing.png"))
+                .body("data.cards[0].title", equalTo("포메라니안"))
+                .body("data.cards[0].tag", equalTo("실종신고"))
+                .body("data.cards[0].date", equalTo("2024-10-05"))
+                .body("data.cards[0].location", equalTo("서울시 강남구"))
+                .body("data.cards[0].interest", equalTo(true))
+                .body("data.cards[1].reportId", equalTo(1))
+                .body("data.cards[1].thumbnailImageUrl", equalTo("https://img.com/1.png"))
+                .body("data.cards[1].title", equalTo("믹스견"))
+                .body("data.cards[1].tag", equalTo("보호중"))
+                .body("data.cards[1].date", equalTo(LocalDate.now().toString()))
+                .body("data.cards[1].location", equalTo("서울"))
+                .body("data.cards[1].interest", equalTo(true))
+                .body("data.lastId", equalTo(1))
+                .body("data.isLast", equalTo(true));
+    }
+
+
+
+}
