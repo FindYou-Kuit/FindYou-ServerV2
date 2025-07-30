eadfad2d22688460e1fb892c89cb2303eaed991e
ksg <ksg001227@naver.com>
2025-07-30 17:12:07 +0900

    [Feat] #31 시군구 정보 반환 서비스 로직 구현

--- src/main/java/com/kuit/findyou/domain/city/repository/SigunguRepository.java
+++ src/main/java/com/kuit/findyou/domain/city/repository/SigunguRepository.java
@@ -3,5 +3,11 @@ package com.kuit.findyou.domain.city.repository;
 import com.kuit.findyou.domain.city.model.Sigungu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SigunguRepository extends JpaRepository<Sigungu,Long> {
+
+    // SigunguRepository.java
+    List<Sigungu> findBySido_Id(Long sidoId);
+
 }
