650996879b98ca7da1a6bf02e91ddd21d2e0e0ec
ksg <ksg001227@naver.com>
2025-07-31 19:06:12 +0900

    [Fix] #37 품종 정보 반환 관련 스웨거 명세 변경

--- src/main/java/com/kuit/findyou/domain/breed/dto/response/BreedListResponseDTO.java
+++ src/main/java/com/kuit/findyou/domain/breed/dto/response/BreedListResponseDTO.java
@@ -8,29 +8,24 @@ import io.swagger.v3.oas.annotations.media.ArraySchema;
 
 public record BreedListResponseDTO(
 
-        @ArraySchema(
-                schema = @Schema(
-                        description = "강아지 품종 리스트",
-                        example = "[\"골든 리트리버\", \"진도견\"]"
-                )
+        @Schema(
+                description = "강아지 품종 리스트",
+                example = "[\"골든 리트리버\", \"진도견\"]"
         )
         List<String> dogBreedList,
 
-        @ArraySchema(
-                schema = @Schema(
-                        description = "고양이 품종 리스트",
-                        example = "[\"러시안 블루\", \"페르시안\"]"
-                )
+        @Schema(
+                description = "고양이 품종 리스트",
+                example = "[\"러시안 블루\", \"페르시안\"]"
         )
         List<String> catBreedList,
 
-        @ArraySchema(
-                schema = @Schema(
-                        description = "기타 동물 품종 리스트",
-                        example = "[\"기타축종\"]"
-                )
+        @Schema(
+                description = "기타 동물 품종 리스트",
+                example = "[\"기타축종\"]"
         )
         List<String> etcBreedList
 
-) {}
+) {
+}
 
