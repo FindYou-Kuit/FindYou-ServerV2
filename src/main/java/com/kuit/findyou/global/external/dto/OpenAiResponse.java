2c37fd1307d9506bd5f554d1245676db36db3624
ksg <ksg001227@naver.com>
2025-07-31 16:32:12 +0900

    [Chore] #37 클래스명 변경

--- src/main/java/com/kuit/findyou/global/external/dto/OpenAiResponse.java
+++ src/main/java/com/kuit/findyou/global/external/dto/OpenAiResponse.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record GptChatResponse(
+public record OpenAiResponse(
         List<Choice> choices
 ) {
     public record Choice(
--- src/main/java/com/kuit/findyou/global/external/util/OpenAiParser.java
+++ src/main/java/com/kuit/findyou/global/external/util/OpenAiParser.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class GptChatResponseParser {
+public class OpenAiParser {
 
     /**
      * 응답 문자열에서 축종(Species)을 추출
