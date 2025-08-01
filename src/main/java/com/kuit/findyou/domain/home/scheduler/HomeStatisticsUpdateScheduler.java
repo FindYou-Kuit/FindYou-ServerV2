cbd6cf06616a3794ddf39db27acc257f85e2c6c0
JangIkhwan <bluensky0213@gmail.com>
2025-08-01 13:48:05 +0900

    [Comment] #28 주석 수정

--- src/main/java/com/kuit/findyou/domain/home/scheduler/HomeStatisticsUpdateScheduler.java
+++ src/main/java/com/kuit/findyou/domain/home/scheduler/HomeStatisticsUpdateScheduler.java
@@ -12,9 +12,7 @@ import org.springframework.stereotype.Component;
 public class HomeStatisticsUpdateScheduler {
     private final HomeStatisticsService homeStatisticsParsingService;
 
-    /*
-    매 시간마다 파싱
-     */
+    // 매 시간마다 파싱
     @Scheduled(cron = "0 0 * * * *")
     public void execute(){
         homeStatisticsParsingService.updateTotalStatistics();
