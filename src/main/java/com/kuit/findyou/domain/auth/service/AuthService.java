b38dcf7bbdc301a9d80c9a88fd0ab140c6267cf8
JangIkhwan <bluensky0213@gmail.com>
2025-08-03 16:00:14 +0900

    [Feat] #42 서비스에 게스트 로그인 메서드 추가

--- src/main/java/com/kuit/findyou/domain/auth/service/AuthService.java
+++ src/main/java/com/kuit/findyou/domain/auth/service/AuthService.java
@@ -1,8 +1,12 @@
 package com.kuit.findyou.domain.auth.service;
 
+import com.kuit.findyou.domain.auth.dto.GuestLoginRequest;
+import com.kuit.findyou.domain.auth.dto.GuestLoginResponse;
 import com.kuit.findyou.domain.auth.dto.KakaoLoginRequest;
 import com.kuit.findyou.domain.auth.dto.KakaoLoginResponse;
 
 public interface AuthService {
     KakaoLoginResponse kakaoLogin(KakaoLoginRequest request);
+
+    GuestLoginResponse guestLogin(GuestLoginRequest request);
 }
