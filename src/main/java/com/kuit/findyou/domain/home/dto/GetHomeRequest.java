dfad7d3db592ecb8af3827a8ffff0f7acc02c7f3
JangIkhwan <bluensky0213@gmail.com>
2025-08-03 14:10:38 +0900

    [Doc] #28 홈화면 조회 요청 파라미터 명세 추가

--- src/main/java/com/kuit/findyou/domain/home/dto/GetHomeRequest.java
+++ src/main/java/com/kuit/findyou/domain/home/dto/GetHomeRequest.java
@@ -1,10 +1,11 @@
 package com.kuit.findyou.domain.home.dto;
 
 import com.kuit.findyou.domain.home.validation.ValidGetHomeRequest;
+import io.swagger.v3.oas.annotations.Parameter;
 
 @ValidGetHomeRequest
 public record GetHomeRequest(
-        Double lat,
-        Double lng
+        @Parameter Double lat,
+        @Parameter Double lng
 ) {
 }
