c3291f6050abb44aa305c825073c25b3e75c144c
JJUYAAA <jjjjyy020704@naver.com>
2025-08-05 13:46:13 +0900

    [feat] #30 보호소/동물병원 controller 수정, repository에 쿼리 추가

--- src/main/java/com/kuit/findyou/domain/animalProtection/controller/AnimalShelterController.java
+++ src/main/java/com/kuit/findyou/domain/animalProtection/controller/AnimalShelterController.java
@@ -1,12 +1,14 @@
 package com.kuit.findyou.domain.animalProtection.controller;
 
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v2")
+@Tag(name = "Animal Shelter", description = "보호소 및 동물병원 조회 API")
+@RequestMapping("api/v2/informations/shelters-and-hospitals")
 @RequiredArgsConstructor
 public class AnimalShelterController {
 
--- src/main/java/com/kuit/findyou/domain/information/repository/AnimalShelterRepository.java
+++ src/main/java/com/kuit/findyou/domain/information/repository/AnimalShelterRepository.java
@@ -2,6 +2,21 @@ package com.kuit.findyou.domain.information.repository;
 
 import com.kuit.findyou.domain.information.model.AnimalShelter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AnimalShelterRepository extends JpaRepository<AnimalShelter,Long> {
+    @Query("""
+    SELECT a FROM AnimalShelter a
+    WHERE a.id > :lastId
+      AND (:type = 'all' 
+           OR (:type = 'hospital' AND a.shelterName LIKE %:hospital%)
+           OR (:type = 'shelter' AND a.shelterName NOT LIKE %:hospital%))
+      AND (:jurisdiction IS NULL OR a.jurisdiction LIKE %:jurisdiction%)
+    ORDER BY a.id ASC
+    """)
+    List<AnimalShelter> findWithFilter(@Param("lastId") Long lastId, @Param("type") String type, @Param("jurisdiction") String jurisdiction);
+
 }
