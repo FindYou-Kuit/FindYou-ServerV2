2fe206cc677904a8485f6e9ee82b8108f0eb7bb1
JangIkhwan <bluensky0213@gmail.com>
2025-08-07 20:47:18 +0900

    [Feat] #46 퍼사드에 관심동물 목록 조회 메서드 추가

--- src/main/java/com/kuit/findyou/domain/user/service/facade/UserServiceFacade.java
+++ src/main/java/com/kuit/findyou/domain/user/service/facade/UserServiceFacade.java
@@ -1,10 +1,8 @@
 package com.kuit.findyou.domain.user.service.facade;
 
 import com.kuit.findyou.domain.report.dto.response.CardResponseDTO;
-import com.kuit.findyou.domain.user.dto.CheckDuplicateNicknameRequest;
-import com.kuit.findyou.domain.user.dto.CheckDuplicateNicknameResponse;
-import com.kuit.findyou.domain.user.dto.RegisterUserRequest;
-import com.kuit.findyou.domain.user.dto.RegisterUserResponse;
+import com.kuit.findyou.domain.user.dto.*;
+import com.kuit.findyou.domain.user.service.interest_report.InterestReportService;
 import com.kuit.findyou.domain.user.service.query.QueryUserService;
 import com.kuit.findyou.domain.user.service.register.RegisterUserService;
 import com.kuit.findyou.domain.user.service.viewed_reports.ViewedReportsRetrieveService;
@@ -14,7 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserServiceFacade {
-
+    private final InterestReportService interestReportService;
     private final ViewedReportsRetrieveService viewedReportsRetrieveService;
     private final RegisterUserService registerUserService;
     private final QueryUserService queryUserService;
@@ -29,5 +27,9 @@ public class UserServiceFacade {
 
     public CheckDuplicateNicknameResponse checkDuplicateNickname(CheckDuplicateNicknameRequest request) {
         return queryUserService.checkDuplicateNickname(request);
+    }
+
+    public RetrieveInterestAnimalsResponse retrieveInterestAnimals(Long userId, Long lastId) {
+        return interestReportService.retrieveInterestAnimals(userId, lastId, 20);
     }
 }
