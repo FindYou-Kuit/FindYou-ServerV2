2f1bf7e26e5cd195e4f41238eafc74301a0896d4
JangIkhwan <bluensky0213@gmail.com>
2025-08-08 00:08:59 +0900

    [Fix] #46 관심동물 목록 조회 서비스 로직 수정

--- src/main/java/com/kuit/findyou/domain/user/service/interest_report/InterestReportServiceImpl.java
+++ src/main/java/com/kuit/findyou/domain/user/service/interest_report/InterestReportServiceImpl.java
@@ -20,13 +20,20 @@ public class InterestReportServiceImpl implements InterestReportService{
     public RetrieveInterestAnimalsResponse retrieveInterestAnimals(Long userId, Long lastId, int size) {
         log.info("[retrieveInterestAnimals] userId = {}, lastId = {}, size = {}", userId, lastId, size);
         // 유저가 관심 게시글로 등록한 게시글을 최신순으로 페이징하여 조회
-
         List<ReportProjection> interestReportProjections = interestReportRepository.findInterestReportsByCursor(userId, lastId, PageRequest.of(0, size + 1));
-
+        List<ReportProjection> takenWithSize = takeWithSize(size, interestReportProjections);
         return RetrieveInterestAnimalsResponse.from(
-                interestReportProjections.subList(0, 21),
-                interestReportProjections.isEmpty() ? -1L : interestReportProjections.get(interestReportProjections.size() - 1).getReportId(),
-                interestReportProjections.size() > size
+                takenWithSize,
+                getLastId(takenWithSize),
+                interestReportProjections.size() <= size
         );
+    }
+
+    private List<ReportProjection> takeWithSize(int size, List<ReportProjection> interestReportProjections) {
+        return interestReportProjections.size() > size ? interestReportProjections.subList(0, size) : interestReportProjections;
+    }
+
+    private long getLastId(List<ReportProjection> interestReportProjections) {
+        return interestReportProjections.size() > 0 ? interestReportProjections.get(interestReportProjections.size() - 1).getReportId() : -1L;
     }
 }
