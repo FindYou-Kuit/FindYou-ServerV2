5d0d1b8b7eac990a177a274e6111838565ea3d84
JangIkhwan <bluensky0213@gmail.com>
2025-08-08 20:23:18 +0900

    [Refactor] #46 Card 응답 DTO 생성 메서드에 관심글 id 리스트를 인자로 추가

--- src/main/java/com/kuit/findyou/domain/report/factory/CardFactory.java
+++ src/main/java/com/kuit/findyou/domain/report/factory/CardFactory.java
@@ -4,32 +4,21 @@ import com.kuit.findyou.domain.report.dto.response.Card;
 import com.kuit.findyou.domain.report.dto.response.CardResponseDTO;
 import com.kuit.findyou.domain.report.dto.response.ReportProjection;
 import com.kuit.findyou.domain.report.model.ReportTag;
-import com.kuit.findyou.domain.report.repository.InterestReportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
 public class CardFactory {
-
-    private final InterestReportRepository interestReportRepository;
-
     public CardResponseDTO createCardResponse(
             List<ReportProjection> projections,
-            Long userId,
+            Set<Long> interestIds,
             Long lastId,
             boolean isLast
     ) {
-        List<Long> reportIds = projections.stream()
-                .map(ReportProjection::getReportId)
-                .toList();
-
-        Set<Long> interestIds = new HashSet<>(interestReportRepository.findInterestedReportIdsByUserIdAndReportIds(userId, reportIds));
-
         List<Card> cards = projections.stream()
                 .map(p -> new Card(
                         p.getReportId(),
--- src/main/java/com/kuit/findyou/domain/report/service/retrieve/ReportRetrieveServiceImpl.java
+++ src/main/java/com/kuit/findyou/domain/report/service/retrieve/ReportRetrieveServiceImpl.java
@@ -5,6 +5,7 @@ import com.kuit.findyou.domain.report.dto.response.CardResponseDTO;
 import com.kuit.findyou.domain.report.dto.response.ReportProjection;
 import com.kuit.findyou.domain.report.factory.CardFactory;
 import com.kuit.findyou.domain.report.model.ReportTag;
+import com.kuit.findyou.domain.report.repository.InterestReportRepository;
 import com.kuit.findyou.domain.report.repository.ReportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -14,7 +15,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
@@ -22,6 +25,7 @@ import java.util.List;
 public class ReportRetrieveServiceImpl implements ReportRetrieveService {
 
     private final ReportRepository reportRepository;
+    private final InterestReportRepository interestReportRepository;
     private final CardFactory cardFactory;
 
     @Override
@@ -46,12 +50,23 @@ public class ReportRetrieveServiceImpl implements ReportRetrieveService {
 
         Long lastId = findLastId(reportSlice);
 
+        Set<Long> interestIds = findInterestIds(userId, reportSlice);
+
         return cardFactory.createCardResponse(
                 reportSlice.getContent(),
-                userId,
+                interestIds,
                 lastId,
                 !reportSlice.hasNext()
         );
+    }
+
+    private Set<Long> findInterestIds(Long userId, Slice<ReportProjection> reportSlice) {
+        List<Long> interestIds = interestReportRepository.findInterestedReportIdsByUserIdAndReportIds(userId,
+                reportSlice.getContent()
+                        .stream()
+                        .map(ReportProjection::getReportId)
+                        .toList());
+        return new HashSet<>(interestIds);
     }
 
     private List<ReportTag> createTagList(ReportViewType reportViewType) {
