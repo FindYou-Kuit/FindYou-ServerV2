f5b85a29fe77914b25b72ae0c4f596ac1645b7c7
JJUYAAA <jjjjyy020704@naver.com>
2025-08-10 14:25:50 +0900

    [refactor] #30 lastId null허용

--- src/main/java/com/kuit/findyou/domain/information/repository/AnimalShelterRepository.java
+++ src/main/java/com/kuit/findyou/domain/information/repository/AnimalShelterRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface AnimalShelterRepository extends JpaRepository<AnimalShelter,Long> {
     @Query("""
     SELECT a FROM AnimalShelter a
-    WHERE a.id > :lastId
+    WHERE (:lastId IS NULL OR a.id > :lastId) 
       AND (
           :type = 'all' 
           OR (:type = 'hospital' AND a.shelterName LIKE CONCAT('%', :hospital, '%'))
@@ -29,7 +29,7 @@ public interface AnimalShelterRepository extends JpaRepository<AnimalShelter,Lon
 
     @Query("""
     SELECT a FROM AnimalShelter a
-    WHERE a.id > :lastId
+    WHERE (:lastId IS NULL OR a.id > :lastId)
       AND a.latitude IS NOT NULL
       AND a.longitude IS NOT NULL
     ORDER BY a.id ASC
