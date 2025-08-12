afa56698871383e64c5abc1e04472f64d667e579
JangIkhwan <bluensky0213@gmail.com>
2025-08-12 16:25:29 +0900

    [Feat] #55 레포지토리에 조회 메서드 추가

--- src/main/java/com/kuit/findyou/domain/information/repository/VolunteerWorkRepository.java
+++ src/main/java/com/kuit/findyou/domain/information/repository/VolunteerWorkRepository.java
@@ -1,7 +1,11 @@
 package com.kuit.findyou.domain.information.repository;
 
 import com.kuit.findyou.domain.information.model.VolunteerWork;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VolunteerWorkRepository extends JpaRepository<VolunteerWork,Long> {
+    List<VolunteerWork> findAllByIdLessThan(Long id, Pageable pageable);
 }
