477ae9ad1563bdc699e92a1cfa02094763c7b8c4
JangIkhwan <bluensky0213@gmail.com>
2025-08-12 16:26:08 +0900

    [Feat] #55 응답 DTO에 생성 메서드 추가

--- src/main/java/com/kuit/findyou/domain/information/dto/GetVolunteerWorksResponse.java
+++ src/main/java/com/kuit/findyou/domain/information/dto/GetVolunteerWorksResponse.java
@@ -1,12 +1,22 @@
 package com.kuit.findyou.domain.information.dto;
 
+import com.kuit.findyou.domain.information.model.VolunteerWork;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record GetVolunteerWorksResponse(
     List<VolunteerWorkDTO> volunteerWorks,
     Long lastId,
     boolean isLast
 ) {
+    public static GetVolunteerWorksResponse from(List<VolunteerWork> takenWithSize, Long newLastId, boolean isLast) {
+        return new GetVolunteerWorksResponse(takenWithSize.stream()
+                .map(VolunteerWorkDTO::entityToDto)
+                .collect(Collectors.toList())
+                , newLastId, isLast);
+    }
+
     public record VolunteerWorkDTO(
             String institution,
             String recruitmentPeriod,
@@ -14,5 +24,14 @@ public record GetVolunteerWorksResponse(
             String workPeriod,
             String workTime,
             String webLink
-    ){ }
+    ){
+        public static VolunteerWorkDTO entityToDto(VolunteerWork entity){
+            return new VolunteerWorkDTO(entity.getInstitution(),
+                    entity.getRecruitmentStartAt() + " ~ " + entity.getRecruitmentEndAt(),
+                    entity.getAddress(),
+                    entity.getVolunteerStartAt() + " ~ " + entity.getVolunteerEndAt(),
+                    entity.getVolunteerTime(),
+                    entity.getWebLink());
+        }
+    }
 }
