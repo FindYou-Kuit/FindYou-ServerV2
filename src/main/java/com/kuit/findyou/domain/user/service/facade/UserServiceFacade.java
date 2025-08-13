fc312f7ca21064196017696b48291bbb596436b7
ksg <ksg001227@naver.com>
2025-08-13 15:01:35 +0900

    [Feat] #61 퍼사드에 서비스 로직 이식

--- src/main/java/com/kuit/findyou/domain/user/service/facade/UserServiceFacade.java
+++ src/main/java/com/kuit/findyou/domain/user/service/facade/UserServiceFacade.java
@@ -2,6 +2,7 @@ package com.kuit.findyou.domain.user.service.facade;
 
 import com.kuit.findyou.domain.report.dto.response.CardResponseDTO;
 import com.kuit.findyou.domain.user.dto.*;
+import com.kuit.findyou.domain.user.service.change_nickname.ChangeNicknameService;
 import com.kuit.findyou.domain.user.service.interest_report.InterestReportService;
 import com.kuit.findyou.domain.user.service.query.QueryUserService;
 import com.kuit.findyou.domain.user.service.register.RegisterUserService;
@@ -16,6 +17,7 @@ public class UserServiceFacade {
     private final ViewedReportsRetrieveService viewedReportsRetrieveService;
     private final RegisterUserService registerUserService;
     private final QueryUserService queryUserService;
+    private final ChangeNicknameService changeNicknameService;
 
     public CardResponseDTO retrieveViewedAnimals(Long lastId, Long userId) {
         return viewedReportsRetrieveService.retrieveViewedAnimals(lastId, userId);
@@ -31,5 +33,9 @@ public class UserServiceFacade {
 
     public CardResponseDTO retrieveInterestAnimals(Long userId, Long lastId) {
         return interestReportService.retrieveInterestAnimals(userId, lastId, 20);
+    }
+
+    public void changeNickname(Long userId, String newNickname) {
+        changeNicknameService.changeNickName(userId, newNickname);
     }
 }
