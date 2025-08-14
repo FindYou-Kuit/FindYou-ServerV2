731703f97cb7cfa0a3ff13d5ae3b2a372a94295d
ksg <ksg001227@naver.com>
2025-08-14 16:55:59 +0900

    [Fix] #62 누락된 @Transactional 추가

--- src/main/java/com/kuit/findyou/domain/user/service/delete/DeleteUserServiceImpl.java
+++ src/main/java/com/kuit/findyou/domain/user/service/delete/DeleteUserServiceImpl.java
@@ -4,6 +4,7 @@ import com.kuit.findyou.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -12,6 +13,7 @@ public class DeleteUserServiceImpl implements DeleteUserService{
 
     private final UserRepository userRepository;
 
+    @Transactional
     @Override
     public void deleteUser(Long userId) {
         userRepository.deleteById(userId);
