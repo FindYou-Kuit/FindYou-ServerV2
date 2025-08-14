b1e18acc148c03149a933fed6a0243e29a5f2267
JangIkhwan <bluensky0213@gmail.com>
2025-08-14 16:12:07 +0900

    [Feat] #55 봉사활동 외부 API 호출용 클래스 작성

---
+++ src/main/java/com/kuit/findyou/global/external/exception/ExternalApiException.java
@@ -0,0 +1,12 @@
+package com.kuit.findyou.global.external.exception;
+
+public class ExternalApiException extends RuntimeException {
+
+    public ExternalApiException(String message) {
+        super(message);
+    }
+
+    public ExternalApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
