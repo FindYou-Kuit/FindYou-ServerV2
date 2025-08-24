ae43625cccb878f1e6fc7f1061529955191cd075
JangIkhwan <bluensky0213@gmail.com>
2025-08-24 18:01:45 +0900

    [Doc] #76 문의사항 추가 API 명세 작성

--- src/main/java/com/kuit/findyou/domain/inquiry/controller/InquiryController.java
+++ src/main/java/com/kuit/findyou/domain/inquiry/controller/InquiryController.java
@@ -2,8 +2,13 @@ package com.kuit.findyou.domain.inquiry.controller;
 
 import com.kuit.findyou.domain.inquiry.dto.AddInquiryRequest;
 import com.kuit.findyou.domain.inquiry.service.InquiryService;
+import com.kuit.findyou.global.common.annotation.CustomExceptionDescription;
 import com.kuit.findyou.global.common.response.BaseResponse;
 import com.kuit.findyou.global.jwt.annotation.LoginUserId;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,13 +16,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.kuit.findyou.global.common.swagger.SwaggerResponseDescription.DEFAULT;
+
+@Tag(name = "Inquiry", description = "문의사항 관련 API")
 @RequestMapping("/api/v2/inquiries")
 @RequiredArgsConstructor
 @RestController
 public class InquiryController {
     private final InquiryService inquiryService;
+    @Operation(
+            summary = "문의사항 추가 API",
+            description = "문의사항 추가 기능을 수행합니다."
+    )
+    @Parameter(in = ParameterIn.HEADER, required = true, name = "Authorization", description = "API 엑세스 토큰", example = "Bearer asdf1234")
+    @CustomExceptionDescription(DEFAULT)
     @PostMapping
-    public BaseResponse<Void> addInquiry(@LoginUserId Long userId, @RequestBody @Valid AddInquiryRequest request){
+    public BaseResponse<Void> addInquiry(@Parameter(hidden = true) @LoginUserId Long userId,
+                                         @RequestBody @Valid AddInquiryRequest request){
         inquiryService.addInquiry(userId, request);
         return BaseResponse.ok(null);
     }
