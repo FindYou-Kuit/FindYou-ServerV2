edf149a4c412d460d28844daddb354e15a7493d4
JJUYAAA <jjjjyy020704@naver.com>
2025-08-24 18:57:55 +0900

    [Feat] #77 보호부서 서비스 인터페이스 생성

---
+++ src/main/java/com/kuit/findyou/domain/information/service/SyncAnimalDepartmentService.java
@@ -0,0 +1,8 @@
+package com.kuit.findyou.domain.information.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SyncAnimalDepartmentService {
+    void synchronize();
+}
