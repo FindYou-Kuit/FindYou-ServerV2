2118a47ac37d69c7a3d3f65ae0a289067af27848
JJUYAAA <jjjjyy020704@naver.com>
2025-08-29 03:13:17 +0900

    [Feat] #77 시도/시군구 모두 포함하는 AND 토큰 생성

--- src/main/java/com/kuit/findyou/domain/information/repository/AnimalDepartmentRepository.java
+++ src/main/java/com/kuit/findyou/domain/information/repository/AnimalDepartmentRepository.java
@@ -5,14 +5,23 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface AnimalDepartmentRepository extends JpaRepository<AnimalDepartment, Long> {
     // 필터링 조건 없이 가나다 순으로 보여줌
     List<AnimalDepartment> findAllByIdGreaterThanOrderByIdAsc(Long id, Pageable pageable);
 
-    // 시군구 필터링
-    List<AnimalDepartment> findAllByOrganizationContainingAndIdGreaterThanOrderByIdAsc(
+    // 정확하게 일치하는 경우
+    List<AnimalDepartment> findAllByOrganizationEqualsIgnoreCaseAndIdGreaterThanOrderByIdAsc(
+            String organization, Long id, Pageable pageable
+    );
+
+    // AND 토큰(시도 & 시군구 모두 포함) 사용
+    List<AnimalDepartment> findAllByOrganizationContainingIgnoreCaseAndOrganizationContainingIgnoreCaseAndIdGreaterThanOrderByIdAsc(
+            String sidoToken, String sigunguToken, Long id, Pageable pageable
+    );
+
+    // 전체를 substring으로
+    List<AnimalDepartment> findAllByOrganizationContainingIgnoreCaseAndIdGreaterThanOrderByIdAsc(
             String organization, Long id, Pageable pageable
     );
 }
