aea8e0964610e0dd893bf696077bdaad07de99b6
JJUYAAA <jjjjyy020704@naver.com>
2025-08-31 09:34:02 +0900

    [Feat] #88 목격신고글 요청 dto 생성

---
+++ src/main/java/com/kuit/findyou/domain/report/dto/request/CreateWitnessReportRequest.java
@@ -0,0 +1,17 @@
+package com.kuit.findyou.domain.report.dto.request;
+
+import java.util.List;
+
+public record CreateWitnessReportRequest(
+        List<String> imgUrls,
+        String species,
+        String breed,
+        String furColor,
+        String foundDate,  //"yyyy.MM.dd"
+        String significant,
+        String location,
+        Double latitude,
+        Double longitude,
+        String landmark
+) {
+}
