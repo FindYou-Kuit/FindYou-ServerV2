5282e1eac68c6696fa4b6ea4c70dab71fcfaf9f6
ksg <ksg001227@naver.com>
2025-09-08 20:36:02 +0900

    [Test] #115 ApplicationTests 클래스에 TestDatabaseConfig 설정 추가

--- src/test/java/com/kuit/findyou/FindyouApplicationTests.java
+++ src/test/java/com/kuit/findyou/FindyouApplicationTests.java
@@ -1,11 +1,18 @@
 package com.kuit.findyou;
 
+import com.kuit.findyou.global.config.TestDatabaseConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestDatabaseConfig.class)
 class FindyouApplicationTests {
 
 	@Test
