75d838180263ed98a84ec0e0f6bbcadbf1a6fcdf
JangIkhwan <bluensky0213@gmail.com>
2025-09-18 18:36:49 +0900

    [Feat] #117 보호중 동물 관련 ApiClient에 총 보호중 동물 건수 수집 메서드 추가

--- src/main/java/com/kuit/findyou/global/external/client/ProtectingAnimalApiClient.java
+++ src/main/java/com/kuit/findyou/global/external/client/ProtectingAnimalApiClient.java
@@ -1,7 +1,5 @@
 package com.kuit.findyou.global.external.client;
 
-import com.kuit.findyou.domain.image.repository.ReportImageRepository;
-import com.kuit.findyou.domain.report.repository.ProtectingReportRepository;
 import com.kuit.findyou.global.external.dto.ProtectingAnimalApiFullResponse;
 import com.kuit.findyou.global.external.dto.ProtectingAnimalItemDTO;
 import com.kuit.findyou.global.external.properties.ProtectingAnimalApiProperties;
@@ -90,6 +88,23 @@ public class ProtectingAnimalApiClient {
         int totalPages = (int) Math.ceil((double) totalCount / DEFAULT_PAGE_SIZE);
 
         return currentPage >= totalPages;
+    }
+
+    public String fetchRescuedAnimalCount(String bgnde, String endde) {
+        ProtectingAnimalApiFullResponse resp = protectingAnimalRestClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/abandonmentPublic_v2")
+                        .queryParam("serviceKey", properties.apiKey())
+                        .queryParam("bgnde", bgnde)
+                        .queryParam("endde", endde)
+                        .queryParam("_type", "json")
+                        .build())
+                .retrieve()
+                .body(ProtectingAnimalApiFullResponse.class);
+
+        String rescuedAnimalCount = resp.response().body().totalCount();
+
+        return rescuedAnimalCount;
     }
 
 }
