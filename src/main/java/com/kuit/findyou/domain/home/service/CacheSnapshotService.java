f5c9f7b2e6fe4028b166b4cfdc86dc06c199f060
JangIkhwan <bluensky0213@gmail.com>
2025-09-21 17:46:41 +0900

    [Feat] #117 캐시 스냅샷 서비스 구현

---
+++ src/main/java/com/kuit/findyou/domain/home/service/CacheSnapshotService.java
@@ -0,0 +1,54 @@
+package com.kuit.findyou.domain.home.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kuit.findyou.domain.home.dto.GetHomeResponse;
+import com.kuit.findyou.domain.home.repository.CacheSnapshotRepository;
+import com.kuit.findyou.global.common.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.kuit.findyou.global.common.response.status.BaseExceptionResponseStatus.INTERNAL_SERVER_ERROR;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CacheSnapshotService {
+    private final CacheSnapshotRepository cacheSnapshotRepository;
+    private final ObjectMapper objectMapper;
+    private final String REDIS_CACHE_KEY = "home:statistics";
+
+    public void save(GetHomeResponse.TotalStatistics stats) {
+        try{
+            String json = objectMapper.writeValueAsString(stats);
+            // DB에도 캐싱
+            Optional<String> cache = cacheSnapshotRepository.find(REDIS_CACHE_KEY);
+            if(cache.isPresent()){
+                cacheSnapshotRepository.delete(REDIS_CACHE_KEY);
+            }
+            cacheSnapshotRepository.insert(REDIS_CACHE_KEY, json);
+        }
+        catch (JsonProcessingException e){
+            log.error("[getCachedTotalStatistics] json 역직렬화 오류");
+            throw new CustomException(INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    public Optional<GetHomeResponse.TotalStatistics> find() {
+        try{
+            Optional<String> json = cacheSnapshotRepository.find(REDIS_CACHE_KEY);
+            if(json.isEmpty()){
+                return Optional.empty();
+            }
+            return Optional.of(objectMapper.readValue(json.get(), GetHomeResponse.TotalStatistics.class));
+        }
+        catch (JsonProcessingException e){
+            log.error("[getCachedTotalStatistics] json 역직렬화 오류");
+            throw new CustomException(INTERNAL_SERVER_ERROR);
+        }
+    }
+}
