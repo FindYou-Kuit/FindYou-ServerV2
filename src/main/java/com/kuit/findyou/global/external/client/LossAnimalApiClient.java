4b7648c6a1b0614e052773a1a9956c43c2929d4c
JangIkhwan <bluensky0213@gmail.com>
2025-09-23 18:05:34 +0900

    [Feat] #113 실종동물 API에서 통계 수치 집계 중 예외처리 추가

--- src/main/java/com/kuit/findyou/global/external/client/LossAnimalApiClient.java
+++ src/main/java/com/kuit/findyou/global/external/client/LossAnimalApiClient.java
@@ -1,11 +1,18 @@
 package com.kuit.findyou.global.external.client;
 
+import com.kuit.findyou.global.common.exception.CustomException;
 import com.kuit.findyou.global.external.dto.LossInfoServiceApiResponse;
 import com.kuit.findyou.global.external.properties.LossAnimalInfoProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientResponseException;
 
+import static com.kuit.findyou.global.common.response.status.BaseExceptionResponseStatus.HOME_STATISTICS_UPDATE_FAILED;
+
+@Slf4j
 @Component
 public class LossAnimalApiClient {
     private final LossAnimalInfoProperties properties;
@@ -19,17 +26,40 @@ public class LossAnimalApiClient {
     }
 
     public String fetchReportedAnimalCount(String bgnde, String endde) {
-        LossInfoServiceApiResponse resp = lossAnimalInfoRestClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .queryParam("serviceKey", properties.apiKey())
-                        .queryParam("bgnde", bgnde)
-                        .queryParam("endde", endde)
-                        .queryParam("_type", "json")
-                        .build())
-                .retrieve()
-                .body(LossInfoServiceApiResponse.class);
+        log.info("[fetchReportedAnimalCount] (bgnde={}, endde={}) 수치 집계 시작", bgnde, endde);
+        try{
+            LossInfoServiceApiResponse resp = lossAnimalInfoRestClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .queryParam("serviceKey", properties.apiKey())
+                            .queryParam("bgnde", bgnde)
+                            .queryParam("endde", endde)
+                            .queryParam("_type", "json")
+                            .build())
+                    .retrieve()
+                    .body(LossInfoServiceApiResponse.class);
 
-        String reportedAnimalCount = resp.response().body().totalCount();
-        return reportedAnimalCount;
+            if (resp == null || resp.response() == null || resp.response().body() == null || resp.response().body().totalCount() == null) {
+                throw new RuntimeException("외부 API 응답이 비어 있습니다");
+            }
+
+            log.info("[fetchReportedAnimalCount] (bgnde={}, endde={}) 수치 집계 완료", bgnde, endde);
+            String reportedAnimalCount = resp.response().body().totalCount();
+            return reportedAnimalCount;
+        }
+        catch (RestClientResponseException e) {
+            // HTTP 4xx / 5xx
+            log.error("[fetchReportedAnimalCount] (bgnde={}, endde={}) 외부 서버 응답 오류 HTTP = {} body = {}", bgnde, endde, e.getRawStatusCode(), e.getResponseBodyAsString(), e);
+            throw new CustomException(HOME_STATISTICS_UPDATE_FAILED);
+        }
+        catch (ResourceAccessException e) {
+            // 네트워크 오류 / 타임아웃
+            log.error("[fetchReportedAnimalCount] (bgnde={}, endde={}) 네트워크 오류 혹은 타임아웃 ", bgnde, endde, e);
+            throw new CustomException(HOME_STATISTICS_UPDATE_FAILED);
+        }
+        catch (Exception e) {
+            // 그 외 알 수 없는 예외
+            log.error("[fetchReportedAnimalCount] (bgnde={}, endde={}) 수치 집계 실패 ", bgnde, endde);
+            throw new CustomException(HOME_STATISTICS_UPDATE_FAILED);
+        }
     }
 }
