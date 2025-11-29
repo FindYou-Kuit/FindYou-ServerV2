ce850422804578f1c61231a82d59282c7ecbe590
JJUYAAA <jjjjyy020704@naver.com>
2025-11-29 23:03:37 +0900

    [Refactor] #144 보호글 레포지토리 - 랜덤 조회 메서드 추가

--- src/main/java/com/kuit/findyou/domain/report/repository/ProtectingReportRepository.java
+++ src/main/java/com/kuit/findyou/domain/report/repository/ProtectingReportRepository.java
@@ -3,6 +3,8 @@ package com.kuit.findyou.domain.report.repository;
 import com.kuit.findyou.domain.report.model.ProtectingReport;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,5 +18,8 @@ public interface ProtectingReportRepository extends JpaRepository<ProtectingRepo
     Optional<ProtectingReport> findWithImagesById(Long id);
 
     List<ProtectingReport> findByNoticeNumberIn(Set<String> noticeNumbers);
+
+    @Query(value = "SELECT * FROM protecting_reports ORDER BY RAND() LIMIT :limit", nativeQuery = true)
+    List<ProtectingReport> findRandomReports(@Param("limit") int limit);
 }
 
