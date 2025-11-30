69c56f0547c3f123329a44aad8fcdf9dc27bb159
JJUYAAA <jjjjyy020704@naver.com>
2025-11-30 12:25:13 +0900

    [Refactor] #144 보호글 레포지토리 - 랜덤 조회 메서드 파라미터 수정

--- src/main/java/com/kuit/findyou/domain/report/repository/ProtectingReportRepository.java
+++ src/main/java/com/kuit/findyou/domain/report/repository/ProtectingReportRepository.java
@@ -1,10 +1,10 @@
 package com.kuit.findyou.domain.report.repository;
 
 import com.kuit.findyou.domain.report.model.ProtectingReport;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,7 +19,7 @@ public interface ProtectingReportRepository extends JpaRepository<ProtectingRepo
 
     List<ProtectingReport> findByNoticeNumberIn(Set<String> noticeNumbers);
 
-    @Query(value = "SELECT * FROM protecting_reports ORDER BY RAND() LIMIT :limit", nativeQuery = true)
-    List<ProtectingReport> findRandomReports(@Param("limit") int limit);
+    @Query("SELECT p FROM ProtectingReport p ORDER BY function('RAND')")
+    List<ProtectingReport> findRandomReports(Pageable pageable);
 }
 
