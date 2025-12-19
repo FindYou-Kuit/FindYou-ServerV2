d2818c96481d99c640aa5e1ca3969c4f7e02f013
JangIkhwan <bluensky0213@gmail.com>
2025-12-19 16:13:04 +0900

    [Feat] #148 리프레시토큰 레포지토리에 findByUserId 메서드 구현
    - userId로 리프레시토큰을 찾는 메서드를 구현

--- src/main/java/com/kuit/findyou/domain/auth/repository/RedisRefreshTokenRepositoryImpl.java
+++ src/main/java/com/kuit/findyou/domain/auth/repository/RedisRefreshTokenRepositoryImpl.java
@@ -1,15 +1,23 @@
 package com.kuit.findyou.domain.auth.repository;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @Repository
 public class RedisRefreshTokenRepositoryImpl implements RedisRefreshTokenRepository {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    private String key(Long userId) {
+        return "refresh-token:" + userId;
+    }
 
     @Override
     public Optional<String> findByUserId(Long userId) {
-
-        return Optional.empty();
+        String value = redisTemplate.opsForValue().get(key(userId));
+        return Optional.ofNullable(value);
     }
 }
