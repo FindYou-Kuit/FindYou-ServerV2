5cde5cb53f301cd075003c422a8dd1e916fd6d5d
JangIkhwan <bluensky0213@gmail.com>
2025-12-19 19:31:45 +0900

    [Fix] #148 리프레시토큰 캐싱 관련 값을 환경변수로 주입하도록 수정
    - 리프레시토큰 캐싱을 위한 키와 TTL을 환경변수로 주입하도록 수정

--- src/main/java/com/kuit/findyou/domain/auth/repository/RedisRefreshTokenRepositoryImpl.java
+++ src/main/java/com/kuit/findyou/domain/auth/repository/RedisRefreshTokenRepositoryImpl.java
@@ -1,18 +1,24 @@
 package com.kuit.findyou.domain.auth.repository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.time.Duration;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Repository
 public class RedisRefreshTokenRepositoryImpl implements RedisRefreshTokenRepository {
     private final RedisTemplate<String, String> redisTemplate;
+    @Value("${findyou.jwt.expiration-ms.refresh-token}")
+    private Long refreshTokenExpireMs;
+    @Value("${findyou.jwt.refresh-token-redis-key-prefix}")
+    private String refreshTokenKeyPrefix;
 
     private String key(Long userId) {
-        return "refresh-token:" + userId;
+        return refreshTokenKeyPrefix + userId;
     }
 
     @Override
@@ -23,6 +29,6 @@ public class RedisRefreshTokenRepositoryImpl implements RedisRefreshTokenReposit
 
     @Override
     public void save(Long userId, String refreshToken) {
-        redisTemplate.opsForValue().set(key(userId), refreshToken);
+        redisTemplate.opsForValue().set(key(userId), refreshToken, Duration.ofMillis(refreshTokenExpireMs));
     }
 }
