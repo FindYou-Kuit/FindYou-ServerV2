7915494d3798154f3ffeecde7e03abee99af6441
JangIkhwan <bluensky0213@gmail.com>
2026-01-04 20:16:22 +0900

    [Feat] #159 프로필 수정 서비스 삭제

--- src/main/java/com/kuit/findyou/domain/user/service/change_profileImage/ChangeProfileImageService.java
+++
@@ -1,7 +0,0 @@
-package com.kuit.findyou.domain.user.service.change_profileImage;
-
-import com.kuit.findyou.domain.user.dto.request.ChangeProfileImageRequest;
-
-public interface ChangeProfileImageService {
-    void changeProfileImage(Long userId, ChangeProfileImageRequest request);
-}
