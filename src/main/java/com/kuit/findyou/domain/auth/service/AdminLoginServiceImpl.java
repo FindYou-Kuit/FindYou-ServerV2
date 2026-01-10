53d27a8917d3a02f0764a931229b947e05cae71d
JJUYAAA <jjjjyy020704@naver.com>
2026-01-10 22:14:18 +0900

    [Refactor] #158 관리자용 로그인 서비스 코드 수정 - refreshToken 제거

--- src/main/java/com/kuit/findyou/domain/auth/service/AdminLoginServiceImpl.java
+++ src/main/java/com/kuit/findyou/domain/auth/service/AdminLoginServiceImpl.java
@@ -1,7 +1,7 @@
 package com.kuit.findyou.domain.auth.service;
 
 import com.kuit.findyou.domain.auth.dto.response.AdminLoginResponse;
-import com.kuit.findyou.domain.auth.repository.RedisRefreshTokenRepository;
+import com.kuit.findyou.domain.user.model.Role;
 import com.kuit.findyou.domain.user.model.User;
 import com.kuit.findyou.domain.user.repository.UserRepository;
 import com.kuit.findyou.global.common.exception.CustomException;
@@ -17,25 +17,20 @@ import static com.kuit.findyou.global.common.response.status.BaseExceptionRespon
 public class AdminLoginServiceImpl implements AdminLoginService{
     private final JwtUtil jwtUtil;
     private final UserRepository userRepository;
-    private final RedisRefreshTokenRepository redisRefreshTokenRepository;
 
     @Value("${admin.admin-user-id}")
     private Long adminUserId;
 
-    @Value("${admin.refresh-ttl-ms}")
-    private Long adminRefreshTtlMs;
+    @Value("${admin.access-ttl-ms}")
+    private Long adminAccessTtlMs;
 
     @Override
     public AdminLoginResponse adminLogin() {
         User user = userRepository.findById(adminUserId)
                 .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
 
-        String accessToken = jwtUtil.createAccessJwt(user.getId(), user.getRole());
-        String refreshToken = jwtUtil.createRefreshJwt(user.getId(), adminRefreshTtlMs);
+        String accessToken = jwtUtil.createAccessJwt(user.getId(), Role.ADMIN, adminAccessTtlMs);
 
-        // 관리자 계정만 TTL 1년으로 저장
-        redisRefreshTokenRepository.save(user.getId(), refreshToken, adminRefreshTtlMs);
-
-        return new AdminLoginResponse(accessToken, refreshToken);
+        return new AdminLoginResponse(user.getId(), accessToken);
     }
 }
